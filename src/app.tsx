eeabc8689ee3d4829859158d2917bd84ef7fab11
brunno <brunnolinker@gmail.com>
2024-09-15 13:34:26 -0300

    fix: cor do texto alterada na raiz para melhor visualização

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,36 @@
 import logo from './assets/logo-in-orbit.svg'
 import background from './assets/background-illustration.svg'
-import { Plus } from 'lucide-react'
+import { Plus, X } from 'lucide-react'
+import { Button } from './componets/ui/button'
+import { Dialog, DialogClose, DialogContent, DialogTitle, DialogTrigger } from './componets/ui/dialog'
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={background} alt="illustration" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
-      <button type='button' className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600">
-        <Plus className="size-4" />
-        Cadastrar meta
-      </button>
-    </div>
+    <Dialog>
+      <div className="h-screen flex flex-col items-center justify-center gap-8">
+        <img src={logo} alt="in.orbit" />
+        <img src={background} alt="illustration" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora
+          mesmo?
+        </p>
+
+        <DialogTrigger asChild>
+          <Button>
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <DialogContent>
+        <div className='flex items-center justify-between'>
+          <DialogTitle>Cadastrar meta</DialogTitle>
+          <DialogClose>
+            <X className='size-5 text-zinc-600'/>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
   )
 }
