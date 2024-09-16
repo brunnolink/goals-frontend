cb3ecc9c74068bc6c50c7c67e56e70904980dee8
brunno <brunnolinker@gmail.com>
2024-09-15 22:39:47 -0300

    feat: fetch de dados em tela de summary

---
+++ src/componets/summary.tsx
@@ -0,0 +1,131 @@
+import { CheckCircle2, Plus } from 'lucide-react'
+import { DialogTrigger } from '../componets/ui/dialog'
+import { Button } from './ui/button'
+import { InOrbitIcon } from './in-orbit-icon'
+import { Progress, ProgressIndicator } from './ui/progress-bar'
+import { Separator } from './ui/separator'
+import { OutlineButton } from './ui/outline-button'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from '../http/get-summary'
+import dayjs from 'dayjs'
+import ptBR from 'dayjs/locale/pt-BR'
+
+export function Summary() {
+  dayjs.locale(ptBR)
+
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60, //60 seconds
+  })
+
+  if(!data) {
+    return null
+  }
+
+  const firstDayOfWeek = dayjs().startOf('week').format('D MMM')
+  const lastDayOfWeek = dayjs().endOf('week').format('D MMM')
+
+  const completedPorcentage = data.completed * 100 / data.total
+
+  return (
+    <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-3">
+          <InOrbitIcon />
+          <span className="text-lg font-semibold capitalize">
+            {firstDayOfWeek} - {lastDayOfWeek}
+          </span>
+        </div>
+
+        <DialogTrigger asChild>
+          <Button size="sm">
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+      <div className="flex flex-col gap-3">
+        <Progress value={8} max={15}>
+          <ProgressIndicator style={{ width: '50%' }} />
+        </Progress>
+
+        <div className="flex items-center justify-between text-xs  text-zinc-400">
+          <span>
+            Você completou <span className="text-zinc-100">{data?.completed}</span> de{''}
+            <span className="text-zinc-100">{data?.total}</span> metas nessa semana.
+          </span>
+          <span>58%</span>
+        </div>
+      </div>
+
+      <Separator />
+
+      <div className="flex flex-wrap gap-3 ">
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Meditar
+        </OutlineButton>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Nadar
+        </OutlineButton>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Praticar exercício
+        </OutlineButton>
+        <OutlineButton>
+          <Plus className="size-4 text-zinc-600" />
+          Beber 3L de água por dia
+        </OutlineButton>
+      </div>
+
+      <div className="flex flex-col gap-6">
+        <h2 className="text-xl font-medium">Sua semana</h2>
+
+        <div className="flex flex-col gap-4 ">
+          <h3 className="font-medium">
+            Domingo{' '}
+            <span className="text-zinc-400 text-xs">(10 de Agosto)</span>
+          </h3>
+
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou "
+                <span className="text-zinc-100">Acordar cedo</span>" às
+                <span className="text-zinc-100"> 08:13h</span>
+              </span>
+            </li>
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou "
+                <span className="text-zinc-100">Acordar cedo</span>" às
+                <span className="text-zinc-100"> 08:13h</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-4 ">
+          <h3 className="font-medium">
+            Segunda-feira{' '}
+            <span className="text-zinc-400 text-xs">(11 de Agosto)</span>
+          </h3>
+
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center gap-2">
+              <CheckCircle2 className="size-4 text-pink-500" />
+              <span className="text-sm text-zinc-400">
+                Você completou "
+                <span className="text-zinc-100">Acordar cedo</span>" às
+                <span className="text-zinc-100"> 08:13h</span>
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
